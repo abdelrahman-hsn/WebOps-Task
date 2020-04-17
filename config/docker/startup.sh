159d880939498cbd58bbf98471af1afcac64984f
Abdelrahman HSN <abdelrahmanhsn1@gmail.com>
2020-04-17 03:27:00 +0200

    Fix Docker

--- config/docker/startup.sh
+++ config/docker/startup.sh
@@ -1,13 +1,7 @@
 #! /bin/sh
 
-# Wait for DB services
-sh ./config/docker/wait-for-services.sh
-
-# Prepare DB (Migrate - If not? Create db & Migrate)
+# # Prepare DB (Migrate - If not? Create db & Migrate)
 sh ./config/docker/prepare-db.sh
 
-# Pre-comple app assets
-sh ./config/docker/asset-pre-compile.sh
-
-# Start Application
+# # Start Application
 bundle exec puma -C config/puma.rb
